00f463e099bea9273b9a37d8a7e5c40d115b2fc7
Sarah <sweetiejeremy@hotmail.com>
2017-02-09 17:01:51 -0800

    solution 1

--- Arrays_and_Strings.js
+++ Arrays_and_Strings.js
@@ -3,15 +3,15 @@ function isUnique (str) {
 		return false;
 	} 
 	var charSet = [];
-	for(var i = 0; i < str.length; i++){
+	for(var i = 0; i < str.length; i++) {
 		var val = str.charAt(i);
+		for(var j = 0; j < charSet.length; j++) {
+			if(charSet[j] == val) {
+				return false;
+			} 
+		}	
 		charSet.push(val);
-		if(!charSet[val]) {
-			return false;
-		} else {
-			return true;
-		}
 	}
+console.log("true");
+return charSet;
 }
-
-
