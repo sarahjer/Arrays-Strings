181029ac2559c0ad13c95ae632a259e4cf70a1d7
Sarah <sweetiejeremy@hotmail.com>
2017-02-27 15:13:03 -0800

    Permuations solution

---
+++ Arrays_and_Strings_2.js
@@ -0,0 +1,17 @@
+function isPerm(a, b) {
+	if(a.length != b.length) {
+		return false;
+	} else {
+		var x ={};
+		for (var i = 0; i < a.length; i++){
+			var c = a[i];
+			x[c] = (isNaN(x[c]) ? 1 : x[c] + 1);
+		}
+		console.log(x);
+		for(var j = 0; j < b.length; j++){
+			var d = b[j];
+			x[d]--;
+		}
+		console.log(x);
+	}
+}
