7f8c7bfeb988a90a54c576de5140b242610692ab
Sarah <sweetiejeremy@hotmail.com>
2017-02-28 14:13:59 -0800

    Solution

---
+++ Arrays_and_Strings_4.js
@@ -0,0 +1,12 @@
+function isPalin(str){
+	var x = {}, palSum = 0;
+	for(var i = 0; i < str.length; i++){
+		var ch = str[i];
+		x[ch] = (isNaN(x[ch]) ? 1 : x[ch]+1);
+	}
+	console.log(x);
+	for(var j in x){
+		palSum += x[j] % 2; 	
+	}
+	return palSum < 2;
+}
