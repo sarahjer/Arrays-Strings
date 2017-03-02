3e3bdb445a462fe94e426f9c4243ef3ffcfcb9a9
Sarah <sweetiejeremy@hotmail.com>
2017-03-02 14:53:01 -0800

    String Compress

---
+++ Arrays_and_Strings_6.js
@@ -0,0 +1,12 @@
+function strCompress(str) {
+	var count = 0;
+	var compressedStr = "";
+	for(var i = 0; i < str.length; i++){
+		count++;
+		if(i + 1 > str.length || str.charAt(i) != str.charAt(i + 1)){
+			compressedStr += "" + str.charAt(i) + count;
+			count = 0;
+		}
+	}
+	return compressedStr.length < str.length ? compressedStr : str;
+}
