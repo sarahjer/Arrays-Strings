a558226f131ef75bfc01ba8b587b58b448e41264
Sarah <sweetiejeremy@hotmail.com>
2017-03-06 15:34:13 -0800

    Rotate String

---
+++ Arrays_and_Strings_9.js
@@ -0,0 +1,13 @@
+function strRotate(str1, str2){
+	if(str1.length != str2.length || str1.length == 0){
+		return false;
+	}
+	var str3 = str1 + str1;
+	return isSubstring(str3, str1)
+}
+
+function isSubstring(stra, strb){
+	var string = stra,
+		substring = strb;
+	return string.includes(substring);
+}
