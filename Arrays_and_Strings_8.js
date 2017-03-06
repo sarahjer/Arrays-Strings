44abf35f3573506bf4e017cd34db9684b0ce55da
Sarah <sweetiejeremy@hotmail.com>
2017-03-06 14:36:08 -0800

    Zero Matrix

---
+++ Arrays_and_Strings_8.js
@@ -0,0 +1,40 @@
+function zeroMat(mat){
+	var row = [],
+		column = [];
+		console.log(row);
+		console.log(column);
+	for(var i = 0; i < mat.length; i++){
+		for(var j = 0; j < mat[0].length; j++){
+			if(mat[i][j] == 0){
+				row[i] = true;
+				column[j] = true;
+			}	
+		}
+	}
+
+	for(var i = 0; i < row.length; i++){
+		if(row[i]){
+			zeroRow(mat, i);
+		}
+	}
+
+	for(var j = 0; j < column.length; j++){
+		if(column[j]){
+			zeroColumn(mat, j);
+		}
+	}
+	console.log(mat);
+}
+	function zeroRow(mat, row){
+		for(var j = 0; j < mat[0].length; j++){
+			mat[row][j] = 0;
+		}
+	}
+
+	function zeroColumn(mat, column){
+		for(var i = 0; i < mat.length; i++){
+			mat[i][column] = 0;
+		}
+	}
+
+	
