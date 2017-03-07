6539a9c8b47518fb54130443dcbd7c79876adc2f
Sarah <sweetiejeremy@hotmail.com>
2017-03-07 14:06:30 -0800

    New Substring Method

--- Arrays_and_Strings_9.js
+++ Arrays_and_Strings_9.js
@@ -2,15 +2,38 @@
 
 
 function strRotate(str1, str2){
-	if(str1.length != str2.length || str1.length == 0){
-		return false;
-	}
+	if(str1.length == str2.length && str1.length > 0){
 	var str3 = str1 + str1;
-	return isSubstring(str3, str1)
+	// console.log(str3);
+	return isSubstring(str3, str2);
+	}
+	return false;
 }
-
 function isSubstring(stra, strb){
-	var string = stra,
-		substring = strb;
-	return string.includes(substring);
+	// var string = stra,
+	// 	substring = strb;
+	// return string.includes(substring);
+	var a = false;
+	var i = 0;
+	while(i < stra.length){
+		if(strb.charAt(0) == stra.charAt(i)){
+			a = true;
+			var b = i;
+			break;
+		} else {
+			i++;
+		}
+	}
+	if(!a){
+		return false;
+	} else {
+		for(var j = 0; j < strb.length; j++){
+			if(strb.charAt(j) == stra.charAt(b)){
+				b++;
+			} else {
+				return false;
+			}
+		}
+		return true;
+	}
 }
