2ddf1e3cf5163820d8d17e5a4ac72f8224ca4413
Sarah <sweetiejeremy@hotmail.com>
2017-03-09 15:48:51 -0800

    New Substring method

--- Arrays_and_Strings_9.js
+++ Arrays_and_Strings_9.js
@@ -29,25 +29,3 @@ function isSubstring(stra, strb){
 
 
 
-// function containsString(string1, string2){
-//   var charArray1 = string1.split('');
-//   var charArray2 = string2.split('');
-//   var match = 0;
-//   console.log(charArray1);
-//   console.log(charArray2);
-//   for(var i = 0; i < charArray1.length - charArray2.length + 1; i++){
-//     match = 0;
-//     for(var j = 0; j < charArray2.length; j++){
-//       if(charArray1[i+j] == charArray2[j]){
-//         match++;
-//       }
-//       // console.log(i, j, match, charArray1[i+j], charArray2[j]);
-//       if(match == charArray2.length){
-//         return true;
-//       }
-//     }
-//   }
-//   return false;
-// }
-
-// console.log(containsString(string1, string2));
